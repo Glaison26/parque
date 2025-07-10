ea9a7509f23168e18c2626d4269f20b494211022
Glaison <Glaison26.queiroz@gmail.com>
2025-07-10 14:21:19 -0300

    dev

--- index.php
+++ index.php
@@ -1,26 +1,58 @@
 <!-- back end da página -->
 <?php
 session_start(); // icicio de session
-$_SESSION['controle']='S';
-include("cabecalho.php"); // arquivo de cabeçalho de página
+$_SESSION['controle'] = 'S';
+//include("cabecalho.php"); // arquivo de cabeçalho de página
 ?>
 
 <!-- front end da página -->
 <!DOCTYPE html>
 <html lang="en">
 
+<head>
+    <title>PMS - Secretaria de Meio Ambiente</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script scr="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
+    <link rel="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
+    <link href="https://nightly.datatables.net/css/jquery.dataTables.css" rel="stylesheet" type="text/css" />
+
+    <link rel="shortcut icon" type="imagex/png" href="./images/img_gop.ico">
+
+    <script scr="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script scr="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+
+    <script src="https://cdn.datatables.net/2.1.5/js/dataTables.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</head>
+
 <body>
-    <br><br><br><br>
+    <br><br>
     <div class="container-fluid">
-        <div class="panel default class" align="center">
-            <div class="panel-heading">
-                <img class="rounded mx-auto d-block" class="img-responsive" src="\parque\imagens\prefeitura.png" class="img-fluid" style="height :100px" style="width:200px">
-            </div>
-            <br><br>
-            <a id="insc" class="btn btn-primary btn-lg" href="/parque/cadastro_data_turno.php"><span class="glyphicon glyphicon-edit"></span> Clique aqui para fazer a inscrição</a>
-        </div>
-        <hr>
+<div class="d-flex justify-content-center">
+    <a id="insc"  class="btn btn-secondary btn-lg" href="/parque/cadastro_data_turno.php"><span class="glyphicon glyphicon-edit"></span> Clique aqui para fazer a inscrição</a>
+</div>
     </div>
 </body>
+
+<style>
+    body {
+        background-image: url('ferias_bosque.jpg');
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+    }
+</style>
 
 </html>
